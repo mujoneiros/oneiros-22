b0684434b600a5262003f4279cb3243906ac57de
Somesh Kumar <kumarsomesh.unos@gmail.com>
2022-10-09 17:38:03 +0530

    add faqs page

--- pages/faqs.jsx
+++ pages/faqs.jsx
@@ -1,5 +1,21 @@
 import React from 'react';
+import Image from 'next/image';
+import Header from '../components/Header';
+import FAQsDropDown from '../components/FAQsDropDown';
 
 export default function Faqs() {
-  return <div>faqs</div>;
+  return <div>
+    <Header /><br /><br />
+    <Image
+      src="/introImage.png"
+      width={1920}
+      height={314}
+    />
+    <div style={{"margin": "3% 20%"}}>
+    <h2 style={{"font-size": "3rem", "color": "#3EDCF2"}}>FAQs</h2>
+    <div>
+      <FAQsDropDown />
+    </div>
+    </div>
+  </div>;
 }
