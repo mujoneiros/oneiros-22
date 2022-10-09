c0d64163e8801d5d80b9561cae6c35f5f00b85d1
Somesh Kumar <kumarsomesh.unos@gmail.com>
2022-10-09 23:23:26 +0530

    redesign sponsors page

---
+++ components/SponsorImage.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { sponsors } from "../data/sponsors";
+import Image from "next/image";
+
+const SponsorImage = () => {
+  return (
+    <div>
+      {sponsors.map((sponsor, _i) => {
+        return (
+          <Image
+            src={sponsor.image}
+            layout="fill"
+            objectFit="contain"
+            alt="sponsor image"
+            style={{ filter: "brightness(0) invert(1)" }}
+          />
+        );
+      })}
+    </div>
+  );
+};
+
+export default SponsorImage;
--- data/sponsors.js
+++ data/sponsors.js
@@ -1,113 +1,134 @@
 export const sponsors = [
   {
     image: '/sponsors/image1.png',
+    type: "title"
   },
   {
     image: '/sponsors/image2.png',
+    type: "title"
   },
-  {
-    image: '/sponsors/image5.png',
-  },
+  // {
+  //   image: '/sponsors/image5.png',
+  // type: title
+  // },
 
   {
     image: '/sponsors/image7.png',
+    type: "title"
   },
 
   {
     image: '/sponsors/image9.png',
+    type: "title"
   },
   {
     image: '/sponsors/image11.png',
+    type: "title"
   },
   {
     image: '/sponsors/image12.png',
+    type: "title"
   },
-  {
-    image: '/sponsors/image13.png',
-  },
+  // {
+  //   image: '/sponsors/image13.png',
+  // },
   {
     image: '/sponsors/image14.png',
+    type: "title"
   },
   {
     image: '/sponsors/image15.png',
+    type: "title"
   },
-  {
-    image: '/sponsors/image16.png',
-  },
-  {
-    image: '/sponsors/image17.png',
-  },
-  {
-    image: '/sponsors/image18.png',
-  },
+  // {
+  //   image: '/sponsors/image16.png',
+  // },
+  // {
+  //   image: '/sponsors/image17.png',
+  // },
+  // {
+  //   image: '/sponsors/image18.png',
+  // },
   {
     image: '/sponsors/image19.png',
+    type: "associate"
   },
-  {
-    image: '/sponsors/image20.png',
-  },
+  // {
+  //   image: '/sponsors/image20.png',
+  // },
   {
     image: '/sponsors/image21.png',
+    type: "associate"
   },
   {
     image: '/sponsors/image22.png',
+    type: "associate"
   },
 
   {
     image: '/sponsors/image31.png',
+    type: "associate"
   },
   {
     image: '/sponsors/image32.png',
+    type: "associate"
   },
   {
     image: '/sponsors/image33.png',
+    type: "associate"
   },
-  {
-    image: '/sponsors/image34.png',
-  },
+  // {
+  //   image: '/sponsors/image34.png',
+  // },
   {
     image: '/sponsors/image35.png',
+    type: "associate"
   },
-  {
-    image: '/sponsors/image36.png',
-  },
-  {
-    image: '/sponsors/image37.png',
-  },
-  {
-    image: '/sponsors/image38.png',
-  },
-  {
-    image: '/sponsors/image39.png',
-  },
+  // {
+  //   image: '/sponsors/image36.png',
+  // },
+  // {
+  //   image: '/sponsors/image37.png',
+  // },
+  // {
+  //   image: '/sponsors/image38.png',
+  // },
+  // {
+  //   image: '/sponsors/image39.png',
+  // },
   {
     image: '/sponsors/image40.png',
+    type: "co"
   },
-  {
-    image: '/sponsors/image41.png',
-  },
+  // {
+  //   image: '/sponsors/image41.png',
+  // },
   {
     image: '/sponsors/image42.png',
+    type: "co"
   },
-  {
-    image: '/sponsors/image43.png',
-  },
-  {
-    image: '/sponsors/image44.png',
-  },
-  {
-    image: '/sponsors/image45.png',
-  },
-  {
-    image: '/sponsors/image46.png',
-  },
+  // {
+  //   image: '/sponsors/image43.png',
+  // },
+  // {
+  //   image: '/sponsors/image44.png',
+  // },
+  // {
+  //   image: '/sponsors/image45.png',
+  // },
+  // {
+  //   image: '/sponsors/image46.png',
+  // },
   {
     image: '/sponsors/image47.png',
+    type: "co"
   },
   {
     image: '/sponsors/image48.png',
+    type: "co"
   },
   {
     image: '/sponsors/image49.png',
+    type: "co"
   },
 ];
